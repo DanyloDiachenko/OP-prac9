70a4808ddf9e18e9a3958806bfcdba4374c5f362
Danylo <107757498+DanyloDiachenko@users.noreply.github.com>
2024-12-31 15:11:23 +0200

    welcome message

--- index.c
+++ index.c
@@ -2,6 +2,8 @@
 
 int main()
 {
+    printf("Hello. This is a simple program to manage files and records.\n");
+    printf("Please select an option from the menu below.\n");
     menu();
 
     return 0;
