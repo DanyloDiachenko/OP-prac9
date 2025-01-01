655085f01031b63a68ad15549ad8185d7e1f61a2
Danylo <107757498+DanyloDiachenko@users.noreply.github.com>
2025-01-01 21:47:24 +0200

    _

--- functions.h
+++ functions.h
@@ -4,7 +4,6 @@
 void createFile();
 void readFile();
 void deleteFile();
-bool validateFileName(char *filename);
 
 void createRecord();
 void readSingleRecord();
@@ -12,5 +11,15 @@ void updateRecord();
 void sortRecords();
 void insertRecord();
 void deleteRecord();
+
+bool getFileName(char *filename, size_t size, char message[]);
+void getRecordDetails(Record *record);
+int getSortField();
+int getSortOrder();
+bool openAndValidateFile(const char *filename, FILE **file);
+bool validateFileName(char *filename);
+bool validateFileExisting(const char *filename);
+bool validateFileName(char *filename, size_t size);
+bool validateFileSignature(FILE *file);
 
 #endif // FUNCTIONS_H
